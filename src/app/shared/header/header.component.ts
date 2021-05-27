e162cf8602a4a1e5bd3beb869a23cda53370733e
Akshat <akshatdivya03@gmail.com>
2021-05-27 10:53:23 +0530

    add header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppRoutingModule} from "../../app-routing.module";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'mu-header',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
+
+  userName = localStorage.getItem("username");
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/welcome']);
   }
 
 }
