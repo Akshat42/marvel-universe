82ac6cb5e5df28ced8db3625ec56733fc5793703
Akshat <akshatdivya03@gmail.com>
2021-05-27 11:56:59 +0530

    handle empty string in logout()

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router) {
   }
 
-  userName = localStorage.getItem("username") || undefined;
+  userName = localStorage.getItem("username") || "";
 
   ngOnInit(): void {
   }
