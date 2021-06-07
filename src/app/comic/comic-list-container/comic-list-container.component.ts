552af7421a9161dac62ee41649374fcce20c9b19
Akshat42 <akshatdivya03@gmail.com>
2021-06-07 12:09:25 +0530

    add validation messages

--- src/app/comic/comic-list-container/comic-list-container.component.ts
+++ src/app/comic/comic-list-container/comic-list-container.component.ts
@@ -1,10 +1,9 @@
 import {Component, HostListener, OnInit} from '@angular/core';
-import {HeroDataService} from "../../hero/hero-data.service";
 import {Card} from "../../model/card";
 import {ComicDataService} from "../comic-data.service";
 import {UtilService} from "../../service/util.service";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
-import {Comic} from "../../model/comic";
+import {AbstractControl, FormBuilder, Validators} from "@angular/forms";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'mu-comic-list-container',
@@ -21,10 +20,21 @@ export class ComicListContainerComponent implements OnInit {
 
   comics: Card[] = [];
   showComicModal: boolean = false;
+  displayComicNameMessage! :string;
+  displayComicDescMessage!: string;
+  private validationMessages = {
+    'comicName': {
+      'required':"Comic Name is required!"
+    },
+    'comicDesc': {
+      'required': "Comic Description is required!"
+    }
+  }
 
   modalComicFormGroup = this.fb.group({
-    comicId: '',
-    comicName: ''
+    comicId: [{value: Math.ceil(Math.random() * 1000), disabled: true}, Validators.required],
+    comicName: ['', Validators.required],
+    comicDesc:['',Validators.required]
   })
 
   ngOnInit(): void {
@@ -40,6 +50,15 @@ export class ComicListContainerComponent implements OnInit {
         this.utilService.hideLoader();
       }
     )
+    const comicNameControl = this.modalComicFormGroup.get('comicName');
+    comicNameControl?.valueChanges.subscribe(
+      value => this.setComicNameMessage(comicNameControl)
+    );
+
+    const comicDescControl = this.modalComicFormGroup.get('comicDesc');
+    comicDescControl?.valueChanges.subscribe(
+      value => this.setComicDescMessage(comicDescControl)
+    );
   }
 
   onComicModalClick() {
@@ -50,23 +69,39 @@ export class ComicListContainerComponent implements OnInit {
     this.showComicModal = false;
   }
 
+  setComicNameMessage(control: AbstractControl): void {
+    this.displayComicNameMessage = '';
+    if ((control.touched || control.dirty) && control.errors) {
+      this.displayComicNameMessage = Object.keys(control.errors).map(key => (<any>this.validationMessages.comicName)[key]).join(' ');
+    }
+  }
+
+  setComicDescMessage(control: AbstractControl): void {
+    this.displayComicDescMessage = '';
+    if ((control.touched || control.dirty) && control.errors) {
+      this.displayComicDescMessage = Object.keys(control.errors).map(key => (<any>this.validationMessages.comicDesc)[key]).join(' ');
+    }
+  }
+
   @HostListener('document:click', ['$event'])
   clickOnWindow(event: any) {
-    if (event.target.className == "card-container" || event.target.className === "card") {
+    if (event.target.className == "card-container") {
       this.showComicModal = false;
     }
   }
 
   addComicDetail() {
-    let comicId = this.modalComicFormGroup.get('comicId')?.value;
-    let comicName = this.modalComicFormGroup.get('comicName')?.value;
-    let newComic: Card = {
-      id: comicId,
-      imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/c0/4bc66d78f1bee.jpg",
-      cardName: comicName
+    if(this.modalComicFormGroup.valid) {
+      let comicId = this.modalComicFormGroup.get('comicId')?.value;
+      let comicName = this.modalComicFormGroup.get('comicName')?.value;
+      let newComic: Card = {
+        id: comicId,
+        imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/c0/4bc66d78f1bee.jpg",
+        cardName: comicName
+      }
+      this.comics.unshift(newComic);
+      this.hideModal();
     }
-    this.comics.unshift(newComic);
-    this.hideModal();
   }
 
 }
